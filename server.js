5764ec739853bbc93afc0fd7e91a71c2c84cdb04
Jiyeon <yeeoni_@naver.com>
2022-03-26 22:33:34 +0900

    [04] post 요청

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(8080, function () {
   console.log('listening on 8080');
@@ -7,4 +8,13 @@ app.listen(8080, function () {
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/write', function (req, res) {
+  res.sendFile(__dirname + '/write.html');
+});
+
+app.post('/add', function (req, res) {
+  res.send('전송완료');
+  console.log(req.body.title);
 });
