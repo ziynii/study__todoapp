e812efea0deb14de6d05e25197bf443f39d50cc7
Jiyeon <yeeoni_@naver.com>
2022-03-26 17:14:10 +0900

    [01] express setting, get 요청 방법

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.listen(8080, function () {
+  console.log('listening on 8080');
+});
+
+app.get('/pet', function (req, res) {
+  res.send('펫용품 쇼핑할 수 있는 사이트입니다');
+});
+
+app.get('/beauty', function (req, res) {
+  res.send('뷰티용품 쇼핑할 수 있는 사이트입니다');
+});
