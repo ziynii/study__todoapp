aff47f36fafb039adf693593518e5674da2a3df4
Jiyeon <yeeoni_@naver.com>
2022-03-27 00:22:37 +0900

    [05] mongoDB로 post요청

--- server.js
+++ server.js
@@ -2,9 +2,29 @@ const express = require('express');
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(8080, function () {
-  console.log('listening on 8080');
-});
+const MongoClient = require('mongodb').MongoClient;
+
+let db;
+MongoClient.connect(
+  'mongodb+srv://ziynii:qorwl0523@cluster0.cbosi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useUnifiedTopology: true },
+  function (error, client) {
+    if (error) return console.log(error);
+
+    db = client.db('todoapp');
+
+    db.collection('post').insertOne(
+      { 이름: 'John', _id: 20 },
+      function (error, result) {
+        console.log('저장완료');
+      }
+    );
+
+    app.listen('8080', function () {
+      console.log('listening on 8080');
+    });
+  }
+);
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -16,5 +36,14 @@ app.get('/write', function (req, res) {
 
 app.post('/add', function (req, res) {
   res.send('전송완료');
-  console.log(req.body.title);
+
+  db.collection('post').insertOne(
+    {
+      title: req.body.title,
+      date: req.body.date,
+    },
+    function (error, result) {
+      console.log('저장완료2');
+    }
+  );
 });
