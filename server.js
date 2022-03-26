0bc5cd4008050430186461acefb91ab6ae6dcaf4
Jiyeon <yeeoni_@naver.com>
2022-03-27 02:43:05 +0900

    [06] ejs로 list.ejs에 데이터 전달

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
 const MongoClient = require('mongodb').MongoClient;
 
@@ -12,13 +13,6 @@ MongoClient.connect(
     if (error) return console.log(error);
 
     db = client.db('todoapp');
-
-    db.collection('post').insertOne(
-      { 이름: 'John', _id: 20 },
-      function (error, result) {
-        console.log('저장완료');
-      }
-    );
 
     app.listen('8080', function () {
       console.log('listening on 8080');
@@ -42,8 +36,15 @@ app.post('/add', function (req, res) {
       title: req.body.title,
       date: req.body.date,
     },
-    function (error, result) {
-      console.log('저장완료2');
-    }
+    function (error, result) {}
   );
+});
+
+app.get('/list', function (req, res) {
+  db.collection('post')
+    .find()
+    .toArray(function (error, result) {
+      console.log(result);
+      res.render('list.ejs', { posts: result });
+    });
 });
